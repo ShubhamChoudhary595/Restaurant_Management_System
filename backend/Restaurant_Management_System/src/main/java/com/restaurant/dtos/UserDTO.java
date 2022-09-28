382e4c6c71cbd38a40a6b58cdc87c27ed6baf8b2
Shubham Choudhary <48170465+ShubhamChoudhary595@users.noreply.github.com>
2022-09-28 15:18:48 +0530

    Add files via upload

---
+++ backend/Restaurant_Management_System/src/main/java/com/restaurant/dtos/UserDTO.java
@@ -0,0 +1,94 @@
+package com.restaurant.dtos;
+
+
+public class  UserDTO {
+
+	private int userId;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+	private String role;
+	private String currentStatus="Enabled";
+	
+	public  UserDTO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public UserDTO(int userId, String name, String email, String phone, String password,String role, String currentStatus) {
+		super();
+		this.userId = userId;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+		this.role = role;
+		this.currentStatus = currentStatus;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getCurrentStatus() {
+		return currentStatus;
+	}
+
+	public void setCurrentStatus(String currentStatus) {
+		this.currentStatus = currentStatus;
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"UserDTO [userId=%s, name=%s, email=%s, phone=%s, password=%s, role=%s, currentStatus=%s]",
+				userId, name, email, phone, password, role, currentStatus);
+	}
+	
+	
+	
+}
